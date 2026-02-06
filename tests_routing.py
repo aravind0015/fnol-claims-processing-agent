7cfbc5ca20051fdc0d3a8cc9e66117673b01d803
aravind0015 <dumpaaravindreddy@gmail.com>
2026-02-06 19:25:29 +0530

    remove tests

--- tests_routing.py
+++
@@ -1,7 +0,0 @@
-"""
-Backwards-compatible routing demo runner.
-
-The refactored deterministic routing demo lives in `tests/test_routing.py`.
-"""
-
-from tests.test_routing import *  # noqa: F403,F401
